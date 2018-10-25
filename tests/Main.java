6355301eec267828a6e542e330806c4fd19db848
Gleny <ggianirodriguez@gmail.com>
2018-10-24 21:36:09 -0300

    Se agrega Log

--- tests/Main.java
+++ tests/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Utilidades.LeerArchivoCSV("C:\\Users\\alu702\\Desktop\\preguntas.csv");
-		
+		System.out.println("LALALAL");
 	}
 
 }
